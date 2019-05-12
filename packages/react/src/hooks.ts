c560c642b9f1bd4a98cb92f5544698eaf903a657
Ahmed T. Ali <ah.tajelsir@gmail.com>
2019-05-12 19:15:36 +0300

    feat(react): add optional deps array to useStore

--- packages/react/src/hooks.ts
+++ packages/react/src/hooks.ts
@@ -10,13 +10,13 @@ type Selector<S> = (state: State) => S;
 /**
  * A hook to get current state and the store's dispatch function
  */
-export function useStore<S>(cb?: Selector<S>): [S, DispatchFunc] {
+export function useStore<S>(cb?: Selector<S>, deps?: any[]): [S, DispatchFunc] {
 	const { state, dispatch } = React.useContext(StoreContext);
 
 	// A memoized callaback
 	const select = React.useCallback(() => {
 		return cb ? cb(state) : state;
-	}, [state, cb]);
+	}, [state, cb, ...(deps || [])]);
 
 	return [select(), dispatch];
 }
